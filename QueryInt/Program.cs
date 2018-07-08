cb82fc527938e84407f2ee68f819bc2dba9574c3
Kuei Peng <ragnakuei@livemail.tw>
2018-07-08 23:46:22 +0800

    加入專案檔案。

---
+++ QueryInt/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Columns;
+using BenchmarkDotNet.Attributes.Jobs;
+using BenchmarkDotNet.Running;
+
+namespace QueryInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<TestRunner>();
+        }
+    }
+
+    [CoreJob] // 可以針對不同的 CLR 進行測試
+    [MinColumn, MaxColumn]
+    [MemoryDiagnoser]
+    public class TestRunner
+    {
+        private readonly TestClass _test = new TestClass();
+
+        public TestRunner()
+        {
+        }
+
+        [Benchmark]
+        public void ListTest() => _test.ListTest();
+
+        [Benchmark]
+        public void ArrayTest() => _test.ArrayTest();
+
+        [Benchmark]
+        public void DictionaryTest() => _test.DictionaryTest();
+    }
+
+    public class TestClass
+    {
+        private readonly List<Int32>          li  = new List<Int32>(1000);
+        private readonly Dictionary<int, int> di  = new Dictionary<int, int>(1000);
+        private readonly int[]                arr = new int[1000];
+        
+        public TestClass()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                li.Add(i);
+                di.Add(i, i);
+                arr[i] = i;
+            }
+        }
+
+        public void ListTest()
+        {
+            var a = li[700];
+        }
+
+        public void ArrayTest()
+        {
+            var a = li[700];
+        }
+
+        public void DictionaryTest()
+        {
+            var a = li[700];
+        }
+    }
+
+}
